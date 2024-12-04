f4795e995235d981dcb5b187c02ae2dcff7a2d31
AakashKainth <aakash.kainth@gmail.com>
2024-12-04 01:16:58 -0500

    JSX explained

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,5 +1,11 @@
+const name = "Timmy";
+
+function displayMessage(){
+    return "Wow!";
+}
 function Hello() {
-    return <h1>Hello from the Hello component!</h1>
+  //return <h1>Her name is {name} {10+22}!</h1>;
+  return <h1>{displayMessage()}</h1>
 }
 
 export default Hello;
