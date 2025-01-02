ed210b213cde4756a7461a42b99285f4998e8ef4
AakashKainth <aakash.kainth@gmail.com>
2025-01-02 13:38:24 -0500

    Pineappple added

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,5 +1,5 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Banana", "Orange"];
+  const fruits = ["Apple", "Mango", "Banana", "Orange","Pineapple"];
   return (
     <div>
       <ul>
